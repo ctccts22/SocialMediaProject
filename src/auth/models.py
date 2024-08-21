cefb131d40ba562d19b4440fdc1535b58e071b91
ctccts22 <ctccts22@gmail.com>
2024-08-21 21:28:20 +0900

    Initial commit

---
+++ src/auth/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Date, DateTime, ForeignKey, Integer, String, Enum
+from datetime import datetime
+
+from .enums import Gender
+from ..database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    name = Column(String)
+    hashed_password = Column(String, nullable=True)
+    created_dt = Column(DateTime, default=datetime.utcnow())
+
+    # profile
+    dob = Column(Date)
+    gender = Column(Enum(Gender))
+    profile_pic = Column(String) # Oracle cloud storage
+    bio = Column(String)
+    location = Column(String)
+
